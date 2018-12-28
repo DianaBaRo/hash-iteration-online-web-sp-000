1c9f8417ecebecf2aa52e332d5781a7f692d0a4a
Diana Badas <diana.badas@gmail.com>
2018-12-28 20:05:41 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -12,7 +12,10 @@ def select_winner(passengers)
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
+    end
   end
-
-  winner
+   
+  winner  
 end
+
+
